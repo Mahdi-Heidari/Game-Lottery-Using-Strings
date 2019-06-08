7f8e2b82e09e004c4aa0990e490321fbde845b87
Mahdi Heidari <mheidarim2001@gmail.com>
2019-06-08 09:14:43 +0430

    Add files via upload

---
+++ LotteryUsingStrings.java
@@ -0,0 +1,76 @@
+package lotteryusingstrings;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class LotteryUsingStrings {
+
+    public static void main(String[] args) {
+
+        // Method One
+        Scanner reader = new Scanner(System.in);
+        Random random = new Random();
+
+        // Generating a lottery as a two-digit string
+        String lottery = ("" + (int) (10 + random.nextFloat() * 89));
+
+        // Prompting the user to enter a guess
+        System.out.print("Enter your lottery pick (two digits) :\t");
+        String guess = reader.next().trim();
+
+        // Checking the guess accuracy
+        while (guess.length() != 2) {
+            System.out.println("Enter a two-digit number:\t");
+            guess = reader.next().trim();
+        }
+        System.out.println("The lottery number is " + lottery);
+
+        // Checking the guess
+        if (guess.equals(lottery)) {
+            System.out.println("Exact match: you win $10,000!");
+        } else if (lottery.contains("" + guess.charAt(0)) && lottery.contains("" + guess.charAt(1))) {
+            System.out.println("Match all digits: you win $3,000!");
+        } else if (lottery.contains("" + guess.charAt(0)) || lottery.contains("" + guess.charAt(1))) {
+            System.out.println("Match one digit: you win $10,000!");
+        } else {
+            System.out.println("Sorry, no match!");
+        }
+
+        // Method Two
+        /*
+         // Generate a lottery as a two-digit string
+         String lottery = "" + (int) (Math.random() * 10)
+         + (int) (Math.random() * 10);
+
+         // Prompt the user to enter a guess
+         Scanner input = new Scanner(System.in);
+         System.out.print("Enter your lottery pick (two digits): ");
+         String guess = input.nextLine();
+
+         // Get digits from lottery
+         char lotteryDigit1 = lottery.charAt(0);
+         char lotteryDigit2 = lottery.charAt(1);
+
+         // Get digits from guess
+         char guessDigit1 = guess.charAt(0);
+         char guessDigit2 = guess.charAt(1);
+         System.out.println("The lottery number is " + lottery);
+
+         // Check the guess
+         if (guess.equals(lottery)) {
+         System.out.println("Exact match: you win $10,000");
+         } else if (guessDigit2 == lotteryDigit1
+         && guessDigit1 == lotteryDigit2) {
+         System.out.println("Match all digits: you win $3,000");
+         } else if (guessDigit1 == lotteryDigit1
+         || guessDigit1 == lotteryDigit2
+         || guessDigit2 == lotteryDigit1
+         || guessDigit2 == lotteryDigit2) {
+         System.out.println("Match one digit: you win $1,000");
+         } else {
+         System.out.println("Sorry, no match");
+         }
+         */
+    }
+
+}
